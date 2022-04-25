41a646ddebcc83a816fc7bd7a046dca8090aeb8c
l3miage-bellatim <bellatif@yahoo.fr>
2022-04-25 20:14:44 +0200

    read by id

--- src/main/java/com/example/controller/UserCRUD.java
+++ src/main/java/com/example/controller/UserCRUD.java
@@ -1,5 +1,6 @@
 package com.example.controller;
-import java.sql.Connection; 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement; 
@@ -19,7 +20,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping; 
 import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException; 
+
+import org.springframework.http.HttpStatus;
 
 @RestController
 @CrossOrigin
@@ -36,6 +40,7 @@ public class UserCRUD {
             ResultSet rs = stmt.executeQuery("SELECT * FROM Chamis");
 
             ArrayList<Chami> L = new ArrayList<Chami>();
+
             while(rs.next()){
                 Chami c = new Chami();
                 c.setLogin(rs.getString("login")); 
@@ -43,8 +48,45 @@ public class UserCRUD {
                 L.add(c);
             }
             return L;
-        }
+
+        } catch (Exception e){
+            response.setStatus(500);
+            try{
+                response.getOutputStream().print(e.getMessage());
+            } catch(Exception e2){
+                System.err.println(e2.getMessage());
+            }
+            return null;
+            }
     }
 
+    @GetMapping("/{userId}")
+    public Chami read(@PathVariable(value="userId") String id, HttpServletResponse response) throws SQLException{
+        try(Connection connection = dataSource.getConnection()){
+            // Statement stmt = connection.createStatement();
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Chamis WHERE login= ? ");
+            stmt.setString(1,id);
+            ResultSet rs = stmt.executeQuery();
+            Chami chami = new Chami();
+
+            while(rs.next()){
+                chami.setLogin(rs.getString("login")); 
+                chami.setAge(rs.getInt("age")); 
+            }
+
+            return chami;
+
+        } catch(Exception e){
+            // throw new ResponseStatusException(NOT_FOUND, "Unable to find resource");
+            response.setStatus(404);
+            try{
+                response.getOutputStream().print(e.getMessage());
+            } catch(Exception e2){
+                System.err.println(e2.getMessage());
+            }
+            return null;
+            }
+        
+    }
 
 }
