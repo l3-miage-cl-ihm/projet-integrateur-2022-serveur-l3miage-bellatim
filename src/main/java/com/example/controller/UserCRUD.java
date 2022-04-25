dda78ed2a0c33ca076cec558f4edc884f291c5fd
l3miage-bellatim <bellatif@yahoo.fr>
2022-04-25 16:39:15 +0200

    allUsers

--- src/main/java/com/example/controller/UserCRUD.java
+++ src/main/java/com/example/controller/UserCRUD.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 import java.sql.Connection; 
-import java.sql.ResultSet; 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement; 
 import java.util.ArrayList; 
 import javax.servlet.http.HttpServletResponse; 
-import javax.sql.DataSource; 
+import javax.sql.DataSource;
+
+import com.example.User;
+import com.example.model.Chami;
+
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.web.bind.annotation.CrossOrigin; 
 import org.springframework.web.bind.annotation.DeleteMapping; 
@@ -24,6 +29,22 @@ public class UserCRUD {
     @Autowired
     private DataSource dataSource;
 
-    
+    @GetMapping("/")
+    public ArrayList<Chami> allUsers(HttpServletResponse response) throws SQLException{
+        try (Connection connection = dataSource.getConnection()){
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Chamis");
+
+            ArrayList<Chami> L = new ArrayList<Chami>();
+            while(rs.next()){
+                Chami c = new Chami();
+                c.setLogin(rs.getString("login")); 
+                c.setAge(rs.getInt("age")); 
+                L.add(c);
+            }
+            return L;
+        }
+    }
+
 
 }
