05578e161e07186039564361d87b3b1d95924fd1
l3miage-bellatim <bellatif@yahoo.fr>
2022-04-27 19:00:28 +0200

    rien

--- src/main/java/com/example/model/Chami.java
+++ src/main/java/com/example/model/Chami.java
@@ -8,7 +8,8 @@ import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 //ajouter lambok
-@Entity@Table(name="chamis", schema="public")
+@Entity
+@Table(name="chamis", schema="public")
 public class Chami {
 
     @Id
