5ceddc5b0a2121104e40609e805b584f3734dcfb
l3miage-bellatim <bellatif@yahoo.fr>
2022-05-02 14:58:02 +0200

    findbychami

--- src/main/java/com/example/repository/DefiRepository.java
+++ src/main/java/com/example/repository/DefiRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DefiRepository extends JpaRepository<Defi, String> {
     
-    List<Defi> findByAuteur(String id);
+    List<Defi> findByAuteur(Chami chami);
 }
     
 
