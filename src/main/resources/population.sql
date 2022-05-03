30bbfd3b5427978ed3d9f319a15a09fa9a538f2f
l3miage-bonninal <bonnin.alice@gmail.com>
2022-05-03 13:16:19 +0200

    yeet \n from .sql

--- src/main/resources/population.sql
+++ src/main/resources/population.sql
@@ -10,19 +10,15 @@ INSERT INTO Chamis (login, age, email) VALUES
 ('yes4moldus', 38, null);
 
 INSERT INTO Defis (id, date_de_creation, description, titre, auteur_login) VALUES
-('D127', '2021-03-15 16:03:00', '- Rendez vous à l''arrêt de bus "Maison du tourisme - Hubert Dubedout".\n- Cherche ce qu''il faut pour faire un méchoui.', 'Le Méchoui', 'carobis'),
-('D145', '2021-04-01 15:03:00', '- Rendez vous à l''arrêt de bus "Grenoble - hôtel de ville".\n
-', 'Le vert, je le mange !', 'escribis'),
+('D127', '2021-03-15 16:03:00', 
+'- Rendez vous à l''arrêt de bus "Maison du tourisme - Hubert Dubedout".
+- Cherche ce qu''il faut pour faire un méchoui.', 'Le Méchoui', 'carobis'),
+('D145', '2021-04-01 15:03:00', 
+'- Rendez vous à l''arrêt de bus "Grenoble - hôtel de ville".', 'Le vert, je le mange !', 'escribis'),
 ('D151', '2021-04-13 12:03:00', 
 '- Rendez-vous à l''arrêt Victor Hugo', 'Ils tournent et rond.', 'nomoldu'),
 ('D189', '2021-03-17 12:03:00', 
-'- Rendez-vous à l''arrêt Victor-Hugo\n
-- C''est l''été. Tu as besoin de grand air.\n
-- Demande à quelqu''un où est la maison de la montagne.\n
-- Longe le tram pour y aller. Tu demanderas où on peut voir des chamois.\n
-- Vas pas trop vite ! Les chamoix sont en plein centre ville !\n
-- Grenoble c''est la ville du béton, mais c''est aussi la capitale des Alpes.\n
-- Fait attention tu pourrais te faire écraser par des pachidermes !\n
-- (Q1) Combien d''écureuils (compter uniquement les tout plats) ?\n
-- (Q2) Combien de chamois ?\n
-- (Q3) Combien d''éléphants ?', 'Et l''écureil alors ?', 'carobis');
+'- Rendez-vous à l''arrêt Victor-Hugo
+- C''est l''été. Tu as besoin de grand air.
+- Demande à quelqu''un où est la maison de la montagne.
+- Longe le tram pour y aller. Tu demanderas où on peut voir des chamois.', 'Et l''écureil alors ?', 'carobis');
