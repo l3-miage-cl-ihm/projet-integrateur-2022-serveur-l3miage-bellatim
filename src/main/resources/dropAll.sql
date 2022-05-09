d2a0a8975a1922f23f191b5f240fba62d1c92e6b
Jean Bou Serhal <jeanbouserhal@outlook.com>
2022-05-09 16:24:07 +0200

    update drop tables script

--- src/main/resources/dropAll.sql
+++ src/main/resources/dropAll.sql
@@ -1,7 +1,8 @@
-drop table chamis cascade;
-drop table defis cascade;
+drop table chami cascade;
+drop table defi cascade;
 drop table visite_joueurs cascade;
 drop table visite cascade;
 drop table reponse cascade;
 drop table etape cascade;
 drop table etape_liste_indice cascade;
+drop table defi_list_etape;
